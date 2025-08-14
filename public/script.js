a2f8dcf5d1591ae274185a905a1c7bddd9cf5b57
SAMEER SINGH CHAUHAN <sameersinghchauhan04@gmail.com>
2025-08-14 13:04:22 +0530

    updating hexx and ascii by bitdata

--- public/script.js
+++ public/script.js
@@ -10,11 +10,14 @@ fileInput.addEventListener('change', function (event) {
         const uint8Array = new Uint8Array(buffer);
         // console.log(uint8Array.length);
 
+        let hex = '',ascii='';
          for (let i = 0; i < uint8Array.length; i += 8) {
             for(let j = 0;j<8 && i+j <uint8Array.length; j++){
                 const bitPosition = i+j;
                 const bitData = uint8Array[bitPosition];
-                
+
+                hex += bitData.toString(16)+ ' ';
+                ascii += bitData >= 32 && bitData <= 126 ? String.fromCharCode(byte) : '.';
             }
          }
     }
