7662df8ae561418aba90e6b12f1ef6d0484e8b5c
SAMEER SINGH CHAUHAN <sameersinghchauhan04@gmail.com>
2025-08-14 12:42:47 +0530

    creating instance of the fileReader-to read file data and also adding onload func on it

--- public/script.js
+++ public/script.js
@@ -4,4 +4,7 @@ fileInput.addEventListener('change', function (event) {
     const file = event.target.files[0];
     if (!file) return;
 
+    const reader = new FileReader();
+    reader.onload = function (e) {
+    }
 });
