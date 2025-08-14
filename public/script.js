db3ec2feccca6d4e8c2358c176006d9cebba95d7
SAMEER SINGH CHAUHAN <sameersinghchauhan04@gmail.com>
2025-08-14 12:34:44 +0530

    ckecking the file...isEmpty and return if empty

--- public/script.js
+++ public/script.js
@@ -2,4 +2,6 @@ const fileInput = document.getElementById('fileInput');
 
 fileInput.addEventListener('change', function (event) {
     const file = event.target.files[0];
+    if (!file) return;
+
 });
