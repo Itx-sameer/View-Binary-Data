904eef5eeeb961c7aaeb3420fe737858559648e8
SAMEER SINGH CHAUHAN <sameersinghchauhan04@gmail.com>
2025-08-14 12:52:00 +0530

    looping thoughout the input data

--- public/script.js
+++ public/script.js
@@ -8,5 +8,9 @@ fileInput.addEventListener('change', function (event) {
     reader.onload = function (e) {
         const buffer = e.target.result;
         const uint8Array = new Uint8Array(buffer);
+        // console.log(uint8Array.length);
+
+         for (let i = 0; i < uint8Array.length; i += 8) {
+         }
     }
 });
